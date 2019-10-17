956c31aa90d81ef841eb297b8c4c267117b7ccef
Matt Soucoup <masoucou@microsoft.com>
2019-10-17 15:07:56 -0700

    Update AppCenterConstants.cs

--- TailwindTraders.Mobile/TailwindTraders.Mobile/TailwindTraders.Mobile/Helpers/AppCenterConstants.cs
+++ TailwindTraders.Mobile/TailwindTraders.Mobile/TailwindTraders.Mobile/Helpers/AppCenterConstants.cs
@@ -6,7 +6,6 @@ namespace TailwindTraders.Mobile.Helpers
 {
     public static class AppCenterConstants
     {
-#error You need App Center secrets to continue
         public readonly static string AndroidAppSecret = "< ENTER YOUR APP CENTER ANDROID SECRET HERE >";
         public readonly static string iOSAppSecret = "< ENTER YOUR APP CENTER IOS SECRET HERE >";
     }
